03edff9da9723f0ede17879ba9cbd947f0b51c02
ernkeyz <exconzy26@gmail.com>
2023-08-30 21:34:19 +0000

    Two-sum leetcode

---
+++ LeetCode/two-sum/two_sum.js
@@ -0,0 +1,16 @@
+const twoSum = (nums, target) => {
+    const numIndices = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;
+
+        if (numIndices.has(complement)) {
+            return [numIndices.get(complement), i];
+        }
+
+        numIndices.set(num, i);
+    }
+
+    return [];
+}
