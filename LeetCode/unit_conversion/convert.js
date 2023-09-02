b8354aef4caedfb97a81dd111ac807abc736b5c4
ernkeyz <exconzy26@gmail.com>
2023-09-02 16:01:39 +0000

    Unit Coversion in JavaScript

---
+++ LeetCode/unit_conversion/convert.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+const convertUnit = () => {
+    //declare constants
+    const cm_per_inch = 2.54;
+    const inch_per_feet = 12;
+
+    // declare variables
+    var feet;
+    var inch;
+    var cm;
+
+// accept input for feet
+    rl.question("Enter Lenght(feet): ", (feetInput) => {
+        feet = parseFloat(feetInput)
+        console.log(`Lenght in feet: ${feet}`);
+
+// accept input for  inches
+    rl.question("Enter lenght(inches): ", (inchInput) => {
+        inch =  parseFloat(inchInput)
+        console.log(`Lenght in inches ${inch}`);
+
+        // calculate totalInches
+            var totalInches = (feet * inch_per_feet) + inch;
+
+        // calculate conversion
+            cm = (totalInches * cm_per_inch);
+
+        // output the result
+            console.log(`Lenght in Centimeters: ${cm.toFixed(2)}`);
+
+            rl.close();
+        });
+    });
+};
+
+convertUnit();
