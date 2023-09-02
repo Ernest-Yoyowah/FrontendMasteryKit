0571c3becf9491612d24b7a812525fed26a2a688
ernkeyz <exconzy26@gmail.com>
2023-09-02 21:05:06 +0000

    campare_the_triplets

---
+++ HackerRank/compare-the-triplets/compare-the-triplets.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {
+    let alicePoints = 0;
+    let bobPoints = 0;
+
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alicePoints++;
+        }
+        else if (b[i] > a[i]) {
+            bobPoints++;
+        }
+    }
+    return [alicePoints, bobPoints];
+}
+
+function main() {
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    console.log(result.join(' '));
+}
