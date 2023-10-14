a4a6686be11173d21738313fab8607efd7fecd2d
ernkeyz <exconzy26@gmail.com>
2023-10-14 21:09:54 +0000

    practicals

--- JavaScript/Reverse_a_String/Reverse_a_String.js
+++ JavaScript/Reverse_a_String/Reverse_a_String.js
@@ -1,18 +1,15 @@
 function reverseString(str) {
   // Your code here
-  /*
-      1. initialize a variable to store reversed string
-      2. loop to iterate through
-      3. append char to the storage decleared
-    */
 
   let reversedStr = "";
+
   for (let i = str.length - 1; i >= 0; i--) {
     reversedStr += str[i];
   }
+
   return reversedStr;
 }
 
-const originalString = "9,8,7,6,5,4,3,2,1";
+const originalString = "Hello, world!";
 const reversedString = reverseString(originalString);
 console.log(reversedString); // Output should be "!dlrow ,olleH"
