b2d8bd1931602848f1b5e21a8e847dd651af6b26
ernkeyz <exconzy26@gmail.com>
2023-10-15 22:22:52 +0000

    new

---
+++ JavaScript/palindromes/palindromes.js
@@ -0,0 +1,9 @@
+function isPalindrome(str) {
+  //Your code here
+}
+
+const testString1 = "racecar";
+const testString2 = "hello";
+
+console.log(isPalindrome(testString1)); // Should return true
+console.log(isPalindrome(testString2)); // Should return false
