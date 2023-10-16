91c7be599b4f8b1264e2d45414043be36e1b65de
ernkeyz <exconzy26@gmail.com>
2023-10-16 19:16:27 +0000

    findLongestWord

---
+++ JavaScript/findLongestWord/findLongestWord.js
@@ -0,0 +1,7 @@
+function findLongestWord(str) {
+  // Your code here
+}
+
+const testString = "The quick brown fox jumped over the lazy dog";
+
+console.log(findLongestWord(testString)); // Should return "jumped"
