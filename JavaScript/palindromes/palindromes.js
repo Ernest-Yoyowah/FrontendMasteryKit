24847452ca4ae466ca80aefd68e67c9356689202
ernkeyz <exconzy26@gmail.com>
2023-10-16 18:58:28 +0000

    reverse string

--- JavaScript/palindromes/palindromes.js
+++ JavaScript/palindromes/palindromes.js
@@ -1,8 +1,12 @@
 function isPalindrome(str) {
   //Your code here
+
+  str = str.replace(/\s/g, "").toLowerCase();
+
+  return str == str.split("").reverse().join("");
 }
 
-const testString1 = "racecar";
+const testString1 = "race car";
 const testString2 = "hello";
 
 console.log(isPalindrome(testString1)); // Should return true
