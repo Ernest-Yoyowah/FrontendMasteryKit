0c78d714fff3f2df7a608bbf66b16148fb4aa267
ernkeyz <exconzy26@gmail.com>
2023-10-18 20:22:50 +0000

    sumOfPositives

---
+++ JavaScript/sumOfPositives/sumOfPositives.js
@@ -0,0 +1,14 @@
+function sumOfPositives(numbers) {
+  // Your code here
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      sum += testArray[i];
+    }
+  }
+  return sum;
+}
+
+const testArray = [3, -2, 7, -5, 10];
+
+console.log(sumOfPositives(testArray)); // Should return 20
