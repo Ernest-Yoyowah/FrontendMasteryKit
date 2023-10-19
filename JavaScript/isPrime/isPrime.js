90079cc42371ba3a92903d550dff26837a266c48
ernkeyz <exconzy26@gmail.com>
2023-10-19 20:11:09 +0000

    isPrime

--- JavaScript/isPrime/isPrime.js
+++ JavaScript/isPrime/isPrime.js
@@ -1,5 +1,15 @@
 function isPrime(number) {
   // Your code here
+
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(isPrime(7)); // Should return true
