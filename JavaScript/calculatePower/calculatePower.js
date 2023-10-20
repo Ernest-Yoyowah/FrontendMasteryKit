fc153f6056b4187c2025d28e2126a1a17019322e
ernkeyz <exconzy26@gmail.com>
2023-10-20 20:17:20 +0000

    calculatePower

--- JavaScript/calculatePower/calculatePower.js
+++ JavaScript/calculatePower/calculatePower.js
@@ -1,6 +1,6 @@
 function calculatePower(base, exponent) {
   // Your code here
-  return base ** exponent;
+  return Math.pow(base, exponent);
 }
 
 console.log(calculatePower(2, 3)); // Should return 8 (2^3 = 2 * 2 * 2)
