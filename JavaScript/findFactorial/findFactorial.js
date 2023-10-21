51a8f638225f7c192eaa6470197e44e8500dd094
ernkeyz <exconzy26@gmail.com>
2023-10-21 21:20:40 +0000

    findFactorial

---
+++ JavaScript/findFactorial/findFactorial.js
@@ -0,0 +1,12 @@
+function findFactorial(n) {
+  // Your code here
+  let factorial = 1;
+
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
+}
+
+console.log(findFactorial(5)); // Should return 120 (5! = 5 * 4 * 3 * 2 * 1)
