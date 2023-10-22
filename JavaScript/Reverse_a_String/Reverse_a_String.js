765ce54f033edd8574f6e97517ee22e0b8d7761f
ernkeyz <exconzy26@gmail.com>
2023-10-22 19:33:33 +0000

    findSecondLargest

--- JavaScript/Reverse_a_String/Reverse_a_String.js
+++ JavaScript/Reverse_a_String/Reverse_a_String.js
@@ -1,20 +1,9 @@
 function reverseString(str) {
   // Your code here
-  /*
-        1. initialize a variable to store reversed string
-        2. loop to iterate through
-        3. append char to the storage decleared
-      */
 
-  let reversedStr = "";
-
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-  }
-
-  return reversedStr;
+  str = str.replace(/\s/g, "").toLowerCase();
+  let reversedString = str.split("").reverse().join("");
+  return reversedString;
 }
 
-const originalString = "Hello, world!";
-const reversedString = reverseString(originalString);
-console.log(reversedString); // Output should be "!dlrow ,olleH"
+console.log(reverseString("Hello, world!")); // Should return "!dlrow ,olleH"
