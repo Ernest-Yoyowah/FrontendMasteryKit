9b5f47bdc7fddee81d352978350507eb4124c37c
ernkeyz <exconzy26@gmail.com>
2023-10-24 20:40:44 +0000

    two sum

---
+++ JavaScript/twoSum/twoSum.js
@@ -0,0 +1,14 @@
+function twoSum(nums, target) {
+  // Your code here
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let num1 = nums[i];
+    let num2 = target - num1;
+    if (map.has(num2)) {
+      return [i, map.get[num2]];
+    }
+    map.set(num1, i);
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Should return [0, 1]
