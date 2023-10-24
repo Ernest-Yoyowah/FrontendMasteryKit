710b74d131b96abf7b592bdad49c7dbb76683a2e
ernkeyz <exconzy26@gmail.com>
2023-10-24 20:42:09 +0000

    two sum

--- JavaScript/twoSum/twoSum.js
+++ JavaScript/twoSum/twoSum.js
@@ -5,7 +5,7 @@ function twoSum(nums, target) {
     let num1 = nums[i];
     let num2 = target - num1;
     if (map.has(num2)) {
-      return [i, map.get[num2]];
+      return [i, map.get(num2)];
     }
     map.set(num1, i);
   }
