ead368b76cac5a71db23ee98d2ffca39de720ed5
ernkeyz <exconzy26@gmail.com>
2023-10-25 20:32:43 +0000

    Even or Odd

---
+++ JavaScript/even_or_odd/evenOrOdd.js
@@ -0,0 +1,6 @@
+// Challenge 3: Even or Odd
+function evenOrOdd(number) {}
+
+// Test the function
+console.log(evenOrOdd(4)); // Should print "even"
+console.log(evenOrOdd(7)); // Should print "odd"
