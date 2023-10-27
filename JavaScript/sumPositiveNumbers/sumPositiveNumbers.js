f632c978d7e10324544c05d9f309158b555e2ac9
ernkeyz <exconzy26@gmail.com>
2023-10-27 19:57:14 +0000

    sumPositiveNumbers

--- JavaScript/sumPositiveNumbers/sumPositiveNumbers.js
+++ JavaScript/sumPositiveNumbers/sumPositiveNumbers.js
@@ -1,21 +1,7 @@
 function sumPositiveNumbers(numbers) {
-    // Initialize a variable to store the sum of positive numbers
-    let sum = 0;
-  
-    // Loop through the array
-    for (let i = 0; i < numbers.length; i++) {
-      // Check if the current number is positive
-      if (/* Your condition here */) {
-        // Add the positive number to the sum
-        // Your code here
-      }
-    }
-  
-    // Return the sum of positive numbers
-    // Your code here
-  }
-  
-  // Test the function
-  const testArray = [3, -8, 1, -6, 4, 7, -2];
-  console.log(sumPositiveNumbers(testArray)); // Should return the sum of positive numbers in the array
-  
+  // Your code here
+}
+
+// Test the function
+const testArray = [3, -8, 1, -6, 4, 7, -2];
+console.log(sumPositiveNumbers(testArray)); // Should return the sum of positive numbers in the array
