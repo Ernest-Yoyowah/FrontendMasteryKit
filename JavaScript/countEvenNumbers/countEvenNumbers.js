798a7f74ebaa7b2cea6253f5d5da2f4906b5d548
ernkeyz <exconzy26@gmail.com>
2023-10-27 19:52:57 +0000

    count Even numbers

--- JavaScript/countEvenNumbers/countEvenNumbers.js
+++ JavaScript/countEvenNumbers/countEvenNumbers.js
@@ -1,7 +1,6 @@
 function countEvenNumbers(numbers) {
   // Your code here
   let count = 0;
-
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 == 0) {
       count++;
@@ -9,7 +8,6 @@ function countEvenNumbers(numbers) {
   }
   return count;
 }
-
 const testArray = [3, 8, 1, 6, 4, 7, 2];
 
 console.log(countEvenNumbers(testArray)); // Should return 4
