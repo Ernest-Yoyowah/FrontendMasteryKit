0974931494f9e0b4360e816c06d03caf64480279
ernkeyz <exconzy26@gmail.com>
2023-10-31 20:21:09 +0000

    diagonalDifference

--- HackerRank/diagonalDifference/diagonalDifference.js
+++ HackerRank/diagonalDifference/diagonalDifference.js
@@ -2,62 +2,24 @@
 
 const fs = require("fs");
 
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'diagonalDifference' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
- */
-
 function diagonalDifference(arr) {
-  // Write your code here
   let leftDiagonal = 0;
-  let rigthDiagonal = 0;
+  let rightDiagonal = 0;
 
   for (let i = 0; i < arr.length; i++) {
     leftDiagonal += arr[i][i];
-    rigthDiagonal += arr[i][arr.length - 1 - i];
+    rightDiagonal += arr[i][arr.length - 1 - i];
   }
-  return Math.abs(leftDiagonal - rigthDiagonal);
+  return Math.abs(leftDiagonal - rightDiagonal);
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+// Sample input for testing
+const sampleInput = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+];
 
-  const n = parseInt(readLine().trim(), 10);
-
-  let arr = Array(n);
-
-  for (let i = 0; i < n; i++) {
-    arr[i] = readLine()
-      .replace(/\s+$/g, "")
-      .split(" ")
-      .map((arrTemp) => parseInt(arrTemp, 10));
-  }
-
-  const result = diagonalDifference(arr);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+// Calculate diagonal difference using sample input
+const resultSample = diagonalDifference(sampleInput);
+console.log(resultSample);
