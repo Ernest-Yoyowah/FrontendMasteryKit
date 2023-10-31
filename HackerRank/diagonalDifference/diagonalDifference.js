8b7204b846d005418c1bcf147cdcc478beafdf26
ernkeyz <exconzy26@gmail.com>
2023-10-31 20:22:33 +0000

    diagonalDifference

--- HackerRank/diagonalDifference/diagonalDifference.js
+++ HackerRank/diagonalDifference/diagonalDifference.js
@@ -3,14 +3,7 @@
 const fs = require("fs");
 
 function diagonalDifference(arr) {
-  let leftDiagonal = 0;
-  let rightDiagonal = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    leftDiagonal += arr[i][i];
-    rightDiagonal += arr[i][arr.length - 1 - i];
-  }
-  return Math.abs(leftDiagonal - rightDiagonal);
+  // Your code here
 }
 
 // Sample input for testing
