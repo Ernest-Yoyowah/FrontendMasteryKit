016e4d9e9590daa2c75dc8540c6f08b5c3dbdbe0
ernkeyz <exconzy26@gmail.com>
2023-11-01 21:22:28 +0000

    staircase

---
+++ HackerRank/diagonalDifference/staircase.js
@@ -0,0 +1,7 @@
+function staircase(n) {
+  // Write your code here
+}
+
+// You can test the function by calling it with a specific value of n.
+const n = 6; // Change this to the desired number of rows
+staircase(n);
