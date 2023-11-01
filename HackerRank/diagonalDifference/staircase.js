b1d244135c5ea619fde53f9dc4246d3b2568c3d2
ernkeyz <exconzy26@gmail.com>
2023-11-01 21:24:27 +0000

    staircase

--- HackerRank/diagonalDifference/staircase.js
+++ HackerRank/diagonalDifference/staircase.js
@@ -1,5 +1,10 @@
 function staircase(n) {
   // Write your code here
+  for (let i = 1; i <= n; i++) {
+    const spaces = " ".repeat(n - i);
+    const hashTags = "#".repeat(i);
+    console.log(spaces + hashTags);
+  }
 }
 
 // You can test the function by calling it with a specific value of n.
