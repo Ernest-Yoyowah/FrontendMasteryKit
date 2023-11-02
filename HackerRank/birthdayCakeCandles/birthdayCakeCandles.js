ebbfefbec0b5b3a404c01480e492fb9bacc95912
ernkeyz <exconzy26@gmail.com>
2023-11-02 20:43:06 +0000

    birthdayCakeCandles

---
+++ HackerRank/birthdayCakeCandles/birthdayCakeCandles.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Function to count the number of candles with the maximum height
+function birthdayCakeCandles(candles) {
+  // Write your code here
+}
+
+// Replace this array with your own test data
+const candles = [3, 2, 1, 3];
+
+// Call the function with your test data
+const result = birthdayCakeCandles(candles);
+
+// Print the result to the console
+console.log(result);
