f53289cd6c4e6199900adca0a67ac318abb38e53
ernkeyz <exconzy26@gmail.com>
2023-11-02 20:18:25 +0000

    miniMaxSum - Solution

--- HackerRank/miniMaxSum/miniMaxSum.js
+++ HackerRank/miniMaxSum/miniMaxSum.js
@@ -2,6 +2,20 @@
 
 function miniMaxSum(arr) {
   // Write your code here
+  arr.sort((a, b) => a - b);
+
+  let minSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    minSum += arr[i];
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    maxSum += arr[i];
+  }
+
+  console.log(minSum, maxSum);
 }
 
 const arr = [1, 2, 3, 4, 5]; // Replace with your own array
