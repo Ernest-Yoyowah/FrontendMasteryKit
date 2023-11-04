9615fc2f827c7e6766b24759551ebc6df581dc14
ernkeyz <exconzy26@gmail.com>
2023-11-04 18:52:46 +0000

    findMaxNumber

--- JavaScript/findMaxNumber/findMaxNumber.js
+++ JavaScript/findMaxNumber/findMaxNumber.js
@@ -1,15 +1,10 @@
 function findMaxNumber(numbers) {
   // Your code here
-  let maxNum = numbers[0];
-
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > maxNum) {
-      maxNum = numbers[i];
-    }
-  }
+  let maxNum = Math.max(...numbers);
   return maxNum;
 }
 
 // Example usage:
-console.log(findMaxNumber([10, 5, 15, 20, 3])); // Should output 20
-console.log(findMaxNumber([100, 500, 250, 750])); // Should output 750
+const numbers = [42, 17, 8, 94, 25, 62];
+const maxNumber = findMaxNumber(numbers);
+console.log("The maximum number is:", maxNumber); // This should print the maximum number in the array.
