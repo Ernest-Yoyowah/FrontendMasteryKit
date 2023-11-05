d41706936e7f610a75376c5a08ff5b0326c94c03
ernkeyz <exconzy26@gmail.com>
2023-11-05 19:16:49 +0000

    compareTriplets

---
+++ HackerRank/compareTriplets/compareTriplets.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) {}
+// Write your code here
+// Example usage:
+const a = [5, 6, 7];
+const b = [3, 6, 10];
+
+const result = compareTriplets(a, b);
+console.log(result);
