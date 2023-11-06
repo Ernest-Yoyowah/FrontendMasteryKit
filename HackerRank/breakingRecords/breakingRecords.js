6290ad7ac8e8748ea899e1dfbd196f846255769b
ernkeyz <exconzy26@gmail.com>
2023-11-06 22:29:04 +0000

    Maria's Basketball Records - HackerRank_Solution

--- HackerRank/breakingRecords/breakingRecords.js
+++ HackerRank/breakingRecords/breakingRecords.js
@@ -2,6 +2,23 @@
 
 function breakingRecords(scores) {
   // write your code here
+  let min_score = scores[0];
+  let max_score = scores[0];
+
+  let min_score_count = 0;
+  let max_score_count = 0;
+
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] < min_score) {
+      min_score = scores[i];
+      min_score_count++;
+    } else if (scores[i] > max_score) {
+      max_score = scores[i];
+      max_score_count++;
+    }
+  }
+
+  return [max_score_count, min_score_count];
 }
 
 // Example usage:
