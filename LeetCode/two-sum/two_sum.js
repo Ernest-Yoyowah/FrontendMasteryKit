7cc514948aa33d3c0ab604683f07226a6c46283b
ernkeyz <exconzy26@gmail.com>
2023-11-06 22:41:10 +0000

    Two Sum - LeetCode_Solution

--- LeetCode/two-sum/two_sum.js
+++ LeetCode/two-sum/two_sum.js
@@ -1,17 +1,17 @@
 const nums = [2, 7, 11, 15];
 
 const twoSum = (nums, target) => {
-    const numIndices = new Map();
+  const numIndices = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const complement = target - num;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
 
-        if (numIndices.has(complement)) {
-            return [numIndices.get(complement), i];
-        }
-        numIndices.set(num, i);
+    if (numIndices.has(complement)) {
+      return [numIndices.get(complement), i];
     }
-
-    return [];
-}
+    numIndices.set(num, i);
+  }
+  console.log("Script executed successfully");
+  return [];
+};
