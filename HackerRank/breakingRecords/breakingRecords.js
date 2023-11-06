0e55ac6ce66f4044d1d915fd7b0421457d5c3637
ernkeyz <exconzy26@gmail.com>
2023-11-06 22:23:15 +0000

    Maria's Basketball Records - HackerRank

---
+++ HackerRank/breakingRecords/breakingRecords.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function breakingRecords(scores) {
+  // write your code here
+}
+
+// Example usage:
+const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+const result = breakingRecords(scores);
+console.log(result.join(" "));
