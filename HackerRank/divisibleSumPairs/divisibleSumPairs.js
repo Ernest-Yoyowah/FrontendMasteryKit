f94a2dc4cbf9315f541085c419ac1477581f19cb
ernkeyz <exconzy26@gmail.com>
2023-11-07 20:55:18 +0000

    divisibleSumPairs - HackerRank Solution

--- HackerRank/divisibleSumPairs/divisibleSumPairs.js
+++ HackerRank/divisibleSumPairs/divisibleSumPairs.js
@@ -9,6 +9,17 @@
  */
 function divisibleSumPairs(k, arr) {
   // Write your code here
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if ((arr[i] + arr[j]) % k == 0) {
+        count++;
+      }
+    }
+  }
+
+  return count;
 }
 
 // Example usage:
