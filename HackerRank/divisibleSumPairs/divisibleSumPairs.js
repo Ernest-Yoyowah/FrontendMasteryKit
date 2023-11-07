12987e7bbf3e0869271de2552c35e2232510d256
ernkeyz <exconzy26@gmail.com>
2023-11-07 20:52:35 +0000

    divisibleSumPairs - HackerRank Challenge

---
+++ HackerRank/divisibleSumPairs/divisibleSumPairs.js
@@ -0,0 +1,18 @@
+/**
+ * Problem Statement:
+ * Given an array of integers and a positive integer k, determine the number of pairs
+ * where the sum of the pair elements is divisible by k.
+ *
+ * @param {number} k - A positive integer.
+ * @param {number[]} arr - An array of integers.
+ * @returns {number} - The count of pairs whose sum is divisible by k.
+ */
+function divisibleSumPairs(k, arr) {
+  // Write your code here
+}
+
+// Example usage:
+const arr = [2, 7, 5, 3, 8];
+const k = 10;
+const result = divisibleSumPairs(k, arr);
+console.log("Count of pairs divisible by", k, "is:", result);
