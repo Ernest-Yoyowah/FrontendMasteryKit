618dbfdc4eb0225e5ce629f9bd7006d886fdebf2
ernkeyz <exconzy26@gmail.com>
2023-11-09 21:39:10 +0000

    catAndMouse

---
+++ HackerRank/catAndMouse/catAndMouse.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function catAndMouse(x, y, z) {
+  // write your code here
+}
+
+// Example usage:
+const testCases = [
+  { x: 1, y: 2, z: 3 },
+  { x: 1, y: 3, z: 2 },
+];
+
+for (const testCase of testCases) {
+  const { x, y, z } = testCase;
+  const result = catAndMouse(x, y, z);
+  console.log(result);
+}
