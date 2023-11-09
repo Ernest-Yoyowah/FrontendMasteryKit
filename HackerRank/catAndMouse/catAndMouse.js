821d18b68af1923343654e93489f0a857e8e658f
ernkeyz <exconzy26@gmail.com>
2023-11-09 21:47:08 +0000

    catAndMouse HackerRank - Solution Testing different values

--- HackerRank/catAndMouse/catAndMouse.js
+++ HackerRank/catAndMouse/catAndMouse.js
@@ -17,7 +17,7 @@ function catAndMouse(x, y, z) {
 // Example usage:
 const testCases = [
   { x: 1, y: 2, z: 3 },
-  { x: 1, y: 3, z: 2 },
+  { x: 1, y: 5, z: 2 },
 ];
 
 for (const testCase of testCases) {
