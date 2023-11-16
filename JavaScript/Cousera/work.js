55b65c06160f4858484ad42e015c9be5f4c053ba
ernkeyz <exconzy26@gmail.com>
2023-11-16 11:49:36 +0000

    Try code

--- JavaScript/Cousera/work.js
+++ JavaScript/Cousera/work.js
@@ -1,6 +1,17 @@
-try {
-  throw new Error();
-  console.log("Hello");
-} catch (err) {
-  console.log("Goodbye");
+class Animal {}
+class Dog extends Animal {
+  constructor() {
+    this.noise = "bark";
+  }
+  makeNoise() {
+    return this.noise;
+  }
 }
+class Wolf extends Dog {
+  constructor() {
+    super();
+    this.noise = "growl";
+  }
+}
+var result = new Wolf();
+console.log(result.makeNoise());
