f1da897f5fbb7ccf9b18c9938f505becdfbf8d93
ernkeyz <exconzy26@gmail.com>
2023-11-22 10:36:18 +0000

    Object Key - for of loop

---
+++ JavaScript/objectKey/objectKey.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Mark",
+  sex: "male",
+  age: "41",
+  profession: "Doctor",
+};
+
+console.log(Object.keys(user));
+
+for (let detail of Object.keys(user)) {
+  console.log(`${detail} => ${user[detail]}`);
+}
