d2563f0dea784dccb90f4731453d624d07350b4b
ernkeyz <exconzy26@gmail.com>
2023-11-25 22:40:16 +0000

    utopianTree - HackerRank - Solution

--- HackerRank/utopianTree/utopianTree.js
+++ HackerRank/utopianTree/utopianTree.js
@@ -2,6 +2,16 @@
 
 function utopianTree(n) {
   // Write your code here
+  let height = 1;
+
+  for (let i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      height *= 2;
+    } else {
+      height += 2;
+    }
+  }
+  return height;
 }
 
 // Example usage:
