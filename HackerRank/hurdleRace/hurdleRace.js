46c25ffb924d2528639d953465dae0b71059cab4
ernkeyz <exconzy26@gmail.com>
2023-11-25 11:39:24 +0000

    Hurdle Race - HackerRank

---
+++ HackerRank/hurdleRace/hurdleRace.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function hurdleRace(k, height) {
+  // write your code here
+}
+
+// Example usage:
+const k = 4;
+const height = [1, 6, 3, 5, 2];
+
+const result = hurdleRace(k, height);
+console.log(result);
