c5900586cf4b965ddab5806af0fee0c9561a6466
ernkeyz <exconzy26@gmail.com>
2023-11-25 22:37:34 +0000

    utopianTree - HackerRank

---
+++ HackerRank/utopianTree/utopianTree.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function utopianTree(n) {
+  // Write your code here
+}
+
+// Example usage:
+const n = 4; // Number of cycles
+const result = utopianTree(n);
+console.log(result);
