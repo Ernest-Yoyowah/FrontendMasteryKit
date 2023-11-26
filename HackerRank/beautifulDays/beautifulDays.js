5cc5e9fed769f1c027516b571ba3d3dbf595df9f
ernkeyz <exconzy26@gmail.com>
2023-11-26 21:09:34 +0000

    Beautiful Days at the Movies - HackerRank

---
+++ HackerRank/beautifulDays/beautifulDays.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function beautifulDays(i, j, k) {
+  // Write your code here
+}
+
+// Example usage:
+const i = 20;
+const j = 23;
+const k = 6;
+
+const result = beautifulDays(i, j, k);
+console.log(result);
