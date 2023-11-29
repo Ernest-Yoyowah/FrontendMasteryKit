b1202faf9d077586747d21055ea50613cedcda19
ernkeyz <exconzy26@gmail.com>
2023-11-29 21:19:29 +0000

    Closure - Sample Code(Note that this will throw an error due to the function scope of the sayHi function)

---
+++ JavaScript/closure/closure.js
@@ -0,0 +1,18 @@
+function human() {
+  const name = "Ernest";
+
+  // The outer function does not have access to variables declared in the inner function
+  console.log(innerText); // Error: innerText is not defined in the outer function
+
+  // The inner function has access to the variable 'name' from the outer function
+  function sayHi() {
+    console.log(`Hi, I am ${name}`);
+
+    // Variables declared in the inner function are not accessible in the outer function
+    const innerText = "Inner text";
+  }
+
+  sayHi();
+}
+
+human();
