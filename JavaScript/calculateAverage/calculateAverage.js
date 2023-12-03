34c9ca71d2060492500ad30f8899d544d4f0ca19
ernkeyz <exconzy26@gmail.com>
2023-12-03 20:27:55 +0000

    function that calculates the average of an array of numbers

--- JavaScript/calculateAverage/calculateAverage.js
+++ JavaScript/calculateAverage/calculateAverage.js
@@ -1,8 +1,9 @@
 const calculateAverage = (numbers) => {
-  const sumOFNumbers = numbers.reduce((sum, num) => sum + num);
+  const sumOFNumbers = numbers.reduce((sum, num) => sum + num, 0);
   const average = sumOFNumbers / numbers.length;
   return average;
 };
+
 const arrayNumbers = [1, 2, 3, 5, 5];
 const result = calculateAverage(arrayNumbers);
 console.log(result);
