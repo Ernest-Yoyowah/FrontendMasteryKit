9bba8c8d36a383e019a9ab8d9d588838aef27c31
ernkeyz <exconzy26@gmail.com>
2023-12-04 08:40:36 +0000

    variable declarations

---
+++ JavaScript/variables/variables.js
@@ -0,0 +1,17 @@
+var name = `Ernest`;
+var name = `Nii`;
+/*
+Variables declared with the var keyword can be redeclared and assigned
+*/
+
+let lastname = `Ernest`;
+// let lastname = `Yoyowah`;
+/*
+Variables declared with the let keyword can not be redeclared but can be re-assigned
+*/
+
+const fullname = `Ernest Yoyowah`;
+/*
+Variables declared with the cont keyword cannot be redeclared and re-assigned
+*/
+console.log(name);
