eee884d3279f55f70cbaebf707cdcc1cae19e13c
ernkeyz <exconzy26@gmail.com>
2023-12-04 12:08:52 +0000

    null && undefined

--- JavaScript/variables/variables.js
+++ JavaScript/variables/variables.js
@@ -20,3 +20,9 @@ let age;
 console.log(age, age + 3); // this will return undefined and NaN
 // undefined is the default value given to unassigned variables
 // NaN is given when you try to use mathematical operations on none number data types
+
+// now i will assign null to the variable and you will notice that it wouldn't return undefined or NaN
+
+let ageTwo = null;
+console.log(ageTwo, ageTwo + 3); // this will return null and 3
+// null is the value assigned intensionally to a variable which will be receiving a value later
