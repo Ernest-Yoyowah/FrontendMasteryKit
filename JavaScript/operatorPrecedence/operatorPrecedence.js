d772cfc993008f1403a2cd893e0b74dd0dffa3f2
ernkeyz <exconzy26@gmail.com>
2023-12-05 14:20:42 +0000

    Operator Precedence - Parentheses

--- JavaScript/operatorPrecedence/operatorPrecedence.js
+++ JavaScript/operatorPrecedence/operatorPrecedence.js
@@ -2,5 +2,7 @@
 -There are many operators in JavaScript
 -Every operator has a corresponding precedence number
 -The one with the larger number executes first
--If the precedence is the same, execution order is from left to rigth
+-If the precedence is the same, execution order is from left to right
+
+NOTE: parentheses () overides any precedence. It has the heighest precedence number of 18
 */
