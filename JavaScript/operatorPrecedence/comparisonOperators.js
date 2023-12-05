7707f96abfd3d7222900781826c19486b45b66fa
ernkeyz <exconzy26@gmail.com>
2023-12-05 15:21:42 +0000

    Operator Precedence - Arithmetic Operators

---
+++ JavaScript/operatorPrecedence/comparisonOperators.js
@@ -0,0 +1,8 @@
+// 9	<	Less than	x < y
+// precedence of 9	<=	Less than or equal	x <= y
+// precedence of 9	>	Greater than	x > y
+// precedence of 9	>=	Greater than or equal	x >= Array
+// precedence of 8	==	Equal	x == y
+// precedence of 8	===	Strict equal	x === y
+// precedence of 8	!=	Unequal	x != y
+// precedence of 8	!==	Strict unequal	x !== y
