9295a2b4cb15d93d73c8c37eca0ccb396064eceb
ernkeyz <exconzy26@gmail.com>
2023-12-05 15:28:35 +0000

    Operator Precedence - Assignment Operators

---
+++ JavaScript/operatorPrecedence/assignmentOperators.js
@@ -0,0 +1,24 @@
+// Assignment Operators
+// Assignments are executed after other operations
+
+// precedence of 2	=	Simple Assignment	x = y
+// precedence of 2	:	Colon Assignment	x: 5
+// precedence of 2	+=	Addition Assignment	x += y
+// precedence of 2	-=	Subtraction Assignment	x -= y
+// precedence of 2	*=	Multiplication Assignment	x *= y
+// precedence of 2	**=	Exponentiation Assignment	x **= y
+// precedence of 2	/=	Division Assignment	x /= y
+// precedence of 2	%=	Remainder Assignment	x %= y
+// precedence of 2	<<=	Left Shift Assignment	x <<= y
+// precedence of 2	>>=	Right Shift Assignment	x >>= y
+// precedence of 2	>>>=	Unsigned Right Shift	x >>>= y
+// precedence of 2	&=	Bitwise AND Assignment	x &= y
+// precedence of 2	|=	Bitwise OR Assignment	x |= y
+// precedence of 2	^=	Bitwise XOR Assignment	x ^= y
+// precedence of 2	&&=	Logical AND Assignment	x &&= y
+// precedence of 2	||=	Logical OR Assignment	x ||= y
+// precedence of 2	=>	Arrow	x => y
+// precedence of 2	yield	Pause / Resume	yield x
+// precedence of 2	yield*	Delegate	yield* x
+// precedence of 2	...	Spread	... x
+// precedence of 1	,	Comma	x , y
