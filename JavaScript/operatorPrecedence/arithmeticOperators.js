561a24a3734258dd2480a7084f2507e291703d67
ernkeyz <exconzy26@gmail.com>
2023-12-05 15:15:42 +0000

    Operator Precedence - Arithmetic Operators

---
+++ JavaScript/operatorPrecedence/arithmeticOperators.js
@@ -0,0 +1,18 @@
+// Arithmetic Operators
+// Exponentiations are executed before multiplications
+// Multiplications and divisions are executed before additions and subtractions
+
+// precedence of 13 **	Exponentiation ES2016
+let a = 5;
+a **= 2;
+// console.log(a);
+
+// precedence of 12 * Multiplication
+let b = 5;
+let c = 2;
+let result = b * c;
+// console.log(result);
+
+// precedence of 12	/	Division	10 / 5
+
+// precedence of %	Division Remainder	10 % 5
