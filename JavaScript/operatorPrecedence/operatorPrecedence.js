d529cfcee51c44f56a361b9da7589bb61ae3cca1
ernkeyz <exconzy26@gmail.com>
2023-12-05 14:17:54 +0000

    Operator Precedence

---
+++ JavaScript/operatorPrecedence/operatorPrecedence.js
@@ -0,0 +1,6 @@
+/*
+-There are many operators in JavaScript
+-Every operator has a corresponding precedence number
+-The one with the larger number executes first
+-If the precedence is the same, execution order is from left to rigth
+*/
