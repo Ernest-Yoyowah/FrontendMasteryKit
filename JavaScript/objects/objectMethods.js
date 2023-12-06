c6ae7d21d95ddf0da2243dcca048c70e29757a4c
ernkeyz <exconzy26@gmail.com>
2023-12-06 11:22:27 +0000

    Object Methods - Login

---
+++ JavaScript/objects/objectMethods.js
@@ -0,0 +1,12 @@
+// Adding Methods
+
+const user = {
+  name: "Ernest",
+  major: "Software Engineering",
+  facaulty: "Engineering",
+  login: function () {
+    console.log(`Logged In`);
+  },
+};
+
+user.login();
