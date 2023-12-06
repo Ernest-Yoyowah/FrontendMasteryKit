73c0c6d1798d520cde5d88dc3e04e230bb9c5712
ernkeyz <exconzy26@gmail.com>
2023-12-06 10:56:34 +0000

    Literals and properties

---
+++ JavaScript/objects/objectLiteral.js
@@ -0,0 +1,10 @@
+// Literals and properties
+
+const user = {
+  name: `Ernest`,
+  major: `Software Engineering`,
+  department: `Computing Systems`,
+  level: `200`,
+};
+
+console.log(`Hello ${user.name}`);
