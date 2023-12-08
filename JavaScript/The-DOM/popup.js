ebe4473acda106056a63c1795255b11f3aff8445
ernkeyz <exconzy26@gmail.com>
2023-12-08 11:53:07 +0000

    Adding JavaScript Functionality to display popup

--- JavaScript/The-DOM/popup.js
+++ JavaScript/The-DOM/popup.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup-wrapper");
+const close = document.querySelector(".popup-close");
+
+button.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
