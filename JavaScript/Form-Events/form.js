91cd5cbf8d2cbe67c321f9d67b1a56311830528a
ernkeyz <exconzy26@gmail.com>
2023-12-11 18:48:48 +0000

    Form Validation-keyup event listerner

--- JavaScript/Form-Events/form.js
+++ JavaScript/Form-Events/form.js
@@ -1,13 +1,13 @@
 const form = document.querySelector(".signup-form");
 const feedback = document.querySelector(".feedback");
 
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+// const username = form.username.value;
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Validation
-
-  const username = form.username.value;
-  const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
   if (usernamePattern.test(username)) {
     feedback.textContent = `Username is Valid`;
@@ -20,4 +20,14 @@ form.addEventListener("submit", (e) => {
   // Boolean value of result
   let result = usernamePattern.test(username);
   console.log(result);
+});
+
+// live feedback
+form.username.addEventListener("keyup", (e) => {
+  //   console.log(e.target.value, form.username.value);
+  if (usernamePattern.test(e.target.value)) {
+    form.username.setAttribute("class", "success");
+  } else {
+    form.username.setAttribute("class", "failed");
+  }
 });
