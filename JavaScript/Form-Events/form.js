e67946a1eb80a080c1eb0699e896f1646894278a
ernkeyz <exconzy26@gmail.com>
2023-12-11 14:58:17 +0000

    Form Events - Validation

--- JavaScript/Form-Events/form.js
+++ JavaScript/Form-Events/form.js
@@ -2,10 +2,12 @@ const form = document.querySelector(".signup-form");
 const username = document.querySelector("#username");
 
 form.addEventListener("submit", (e) => {
-  if (username.value > 0) {
-    console.log(`Congratulations ${username.value}`);
-  } else {
-    e.preventDefault();
+  if (username.value.includes("@")) {
+    console.log(`${username.value} is not a valid username`);
+  } else if (username.value <= 0) {
     console.log("No Input");
+    e.preventDefault();
+  } else {
+    console.log(`${username.value}`);
   }
 });
