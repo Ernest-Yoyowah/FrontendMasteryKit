b5d829832cdb4e33589af2b4043a9f7875a3bc27
ernkeyz <exconzy26@gmail.com>
2023-12-11 15:49:39 +0000

    Testing RegEx-search_method - returns the index of the first match value

--- JavaScript/Form-Events/form.js
+++ JavaScript/Form-Events/form.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", (e) => {
 
 // Testing RegEx
 
-const newName = "Ernest";
+const newName = "745455Ernest";
 const pattern = /[a-zA-Z]{6,}/;
 
 let result = pattern.test(newName);
