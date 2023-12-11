6df854c816b362a22be8608f4853943ea2276ddc
ernkeyz <exconzy26@gmail.com>
2023-12-11 15:45:58 +0000

    Testing RegEx-search_method

--- JavaScript/Form-Events/form.js
+++ JavaScript/Form-Events/form.js
@@ -8,8 +8,13 @@ form.addEventListener("submit", (e) => {
 
 // Testing RegEx
 
-const newName = "Ernest";
+const newName = "1Ernest";
 const pattern = /[a-zA-Z]{6,}/;
 
 let result = pattern.test(newName);
 console.log(result);
+
+// RegEx Search Method
+
+let matchValue = newName.search(pattern);
+console.log(matchValue); // Returns the index of the first match value
