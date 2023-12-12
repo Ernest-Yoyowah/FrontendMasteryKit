5fca9af323bfeacdd424a6268082aad2dab73bb0
ernkeyz <exconzy26@gmail.com>
2023-12-12 11:40:10 +0000

    Quiz App-Project-checking correct answer_app.js

---
+++ JavaScript/ninja-quiz/app.js
@@ -0,0 +1,24 @@
+const correctAnswers = ["A", "B", "A", "B"];
+
+const form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  // Check Answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
