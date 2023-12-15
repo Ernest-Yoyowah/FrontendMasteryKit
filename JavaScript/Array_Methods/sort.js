a4f770d709e18e4c2199c67706bc91f5f1f481d4
ernkeyz <exconzy26@gmail.com>
2023-12-15 12:19:44 +0000

    array sort method

---
+++ JavaScript/Array_Methods/sort.js
@@ -0,0 +1,6 @@
+// example 1 - sorting strings
+const names = ["Ernest", "Nii", "Yoyowah", "Mike", "John", "Dan"];
+
+// in-buit to sort in alphabetical order automatically
+names.sort();
+console.log(names);
