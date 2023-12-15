f85d422b8f0a6ad72bbc0b5f250e1f82fc336552
ernkeyz <exconzy26@gmail.com>
2023-12-15 12:11:02 +0000

    array find method

--- JavaScript/Array_Methods/find.js
+++ JavaScript/Array_Methods/find.js
@@ -2,8 +2,6 @@ const scores = [10, 20, 60, 40, 50, 80, 25, 42];
 
 // we want to find the heighest score in the array
 
-const firstHighScore = scores.find((highScore) => {
-  return highScore > 50;
-});
+const firstHighScore = scores.find((highScore) => highScore > 50);
 
 console.log(firstHighScore);
