cf35821b30498cf966b01c4da8eebb2bb41608b9
ernkeyz <exconzy26@gmail.com>
2023-12-15 14:52:45 +0000

    Method Chaining

--- JavaScript/Array_Methods/chainArray.js
+++ JavaScript/Array_Methods/chainArray.js
@@ -1,15 +1,25 @@
 const products = [
-  { namee: "Toyota", price: 50 },
-  { namee: "BMW", price: 30 },
-  { namee: "Benz", price: 5 },
-  { namee: "Nissan", price: 10 },
-  { namee: "SUV", price: 60 },
+  { name: "Toyota", price: 50 },
+  { name: "BMW", price: 30 },
+  { name: "Benz", price: 5 },
+  { name: "Nissan", price: 10 },
+  { name: "SUV", price: 60 },
 ];
 
-const filtered = products.filter((product) => product.price > 20);
+// const filtered = products.filter((product) => product.price > 20);
 
-const promos = filtered.map((product) => {
-  return `the promo price of ${product.namee} is $${product.price / 2}.00`;
-});
+// const promos = filtered.map((product) => {
+//   return `the promo price of ${product.name} is $${product.price / 2}.00`;
+// });
 
-console.log(promos);
+// Method Chaining
+
+const filtered = products
+  .filter((product) => product.price > 20)
+  .map((product) => {
+    return `the promo price of ${product.name} is $${product.price / 2}.00`;
+  });
+
+console.log(filtered);
+
+// console.log(promos);
