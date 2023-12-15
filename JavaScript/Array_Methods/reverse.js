554fd868b93f8d6426e68d358433f3272d746144
ernkeyz <exconzy26@gmail.com>
2023-12-15 12:30:47 +0000

    array sort method- reversing an array of numbers

--- JavaScript/Array_Methods/reverse.js
+++ JavaScript/Array_Methods/reverse.js
@@ -3,4 +3,12 @@ const names = ["Ernest", "Nii", "Yoyowah", "Mike", "John", "Dan"];
 
 // reverses the array
 names.reverse();
-console.log(names);
+// console.log(names);
+
+//example 2 - sorting numbers in reverse
+
+const scores = [10, 20, 60, 40, 50, 80, 25, 42];
+
+// automatically sorts from last to first
+scores.reverse();
+console.log(scores);
