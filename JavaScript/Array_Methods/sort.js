a9e5cfc3bb1bfc01f4ca363ca38736afc536503f
ernkeyz <exconzy26@gmail.com>
2023-12-15 12:20:28 +0000

    array sort method- sorting numbers

--- JavaScript/Array_Methods/sort.js
+++ JavaScript/Array_Methods/sort.js
@@ -4,3 +4,11 @@ const names = ["Ernest", "Nii", "Yoyowah", "Mike", "John", "Dan"];
 // in-buit to sort in alphabetical order automatically
 names.sort();
 console.log(names);
+
+//example 2 - sorting numbers
+
+const scores = [10, 20, 60, 40, 50, 80, 25, 42];
+
+// automatically sorts from lowest to heighest
+scores.sort();
+console.log(scores);
