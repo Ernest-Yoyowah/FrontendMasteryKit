87f75c9ba22ccba1c347d63a99a4104da926e5d9
ernkeyz <exconzy26@gmail.com>
2023-12-16 22:37:14 +0000

    HackerRank-countApplesAndOranges

---
+++ HackerRank/countApplesAndOranges/countApplesAndOranges.js
@@ -0,0 +1,13 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  // write your code here
+}
+
+// Example Usage:
+const s = 7;
+const t = 11;
+const a = 5;
+const b = 15;
+const apples = [-2, 2, 1];
+const oranges = [5, -6];
+
+countApplesAndOranges(s, t, a, b, apples, oranges);
