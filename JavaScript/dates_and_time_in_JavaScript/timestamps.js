f78ec96d1db5508a2b3723b81997782bff64f766
ernkeyz <exconzy26@gmail.com>
2023-12-26 19:25:03 +0000

    TimeStamps, minutes between 2019-2023

--- JavaScript/dates_and_time_in_JavaScript/timestamps.js
+++ JavaScript/dates_and_time_in_JavaScript/timestamps.js
@@ -1,5 +1,5 @@
 const before = new Date("February 1 2019 7:30:59");
-const now = new Date();
+const now = new Date(); // today's date 12/26/2023, 7:24:38 PM
 
 const diff = now.getTime() - before.getTime();
 console.log(diff);
