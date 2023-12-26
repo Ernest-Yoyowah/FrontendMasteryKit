6aa8e87825f36edf249e2eebb254a1645c954bfc
ernkeyz <exconzy26@gmail.com>
2023-12-26 19:42:35 +0000

    Converting timeStamps to Date

--- JavaScript/dates_and_time_in_JavaScript/timestamps.js
+++ JavaScript/dates_and_time_in_JavaScript/timestamps.js
@@ -2,7 +2,7 @@ const before = new Date("January 1 2023 0:00:59");
 const now = new Date(); // today's date 12/26/2023, 7:24:38 PM
 
 const diff = now.getTime() - before.getTime();
-console.log(diff);
+// console.log(diff);
 
 const mins = Math.round(diff / 1000 / 60);
 const hours = Math.round(mins / 60);
@@ -13,4 +13,8 @@ const weeks = Math.round(days / 7);
 // console.log(hours);
 // console.log(days);
 // console.log(weeks);
-console.log(`This blog was written ${days} days ago`);
+// console.log(`This blog was written ${days} days ago`);
+
+// Converting timestamps into date objects
+const timestamps = diff;
+console.log(new Date(timestamps).toLocaleString());
