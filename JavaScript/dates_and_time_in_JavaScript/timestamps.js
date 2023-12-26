e4340ee88dc1d73ddd8c6fe70938d1444a1902de
ernkeyz <exconzy26@gmail.com>
2023-12-26 19:36:47 +0000

    TimeStamps 2023 Jan - 2023 Dec

--- JavaScript/dates_and_time_in_JavaScript/timestamps.js
+++ JavaScript/dates_and_time_in_JavaScript/timestamps.js
@@ -1,4 +1,4 @@
-const before = new Date("February 1 2019 7:30:59");
+const before = new Date("January 1 2023 0:00:59");
 const now = new Date(); // today's date 12/26/2023, 7:24:38 PM
 
 const diff = now.getTime() - before.getTime();
@@ -6,6 +6,11 @@ console.log(diff);
 
 const mins = Math.round(diff / 1000 / 60);
 const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
+const weeks = Math.round(days / 7);
 
 // console.log(mins);
-console.log(hours);
+// console.log(hours);
+// console.log(days);
+// console.log(weeks);
+console.log(`This blog was written ${days} days ago`);
