d5378fbaff35b3c83ee146ad9a835184c56eacc5
ernkeyz <exconzy26@gmail.com>
2023-12-26 18:36:06 +0000

    Date and Time in JavaScript

---
+++ JavaScript/dates_and_time_in_JavaScript/dates.js
@@ -0,0 +1,12 @@
+//dates and time
+const now = new Date();
+
+// console.log(now);
+console.log(typeof now);
+
+// Methods (years, month, day, time)
+
+console.log(`Full Year ${now.getFullYear()}`);
+console.log(`Month ${now.getMonth()}`);
+console.log(`Date ${now.getDate()}`);
+console.log(`Day ${now.getDay()}`);
