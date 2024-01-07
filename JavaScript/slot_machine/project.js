fe13a333451e15a5ebc5d430aa2ed87a570da324
ernkeyz <exconzy26@gmail.com>
2024-01-07 21:18:11 +0000

    Step 1 completed-Deposit some money

--- JavaScript/slot_machine/project.js
+++ JavaScript/slot_machine/project.js
@@ -5,3 +5,24 @@
 // 5. Check if user won
 // 6. Give user award if won else take bet
 // 7. Play again
+
+// GH₵
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a Deposit Amount: ");
+    const numDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numDepositAmount) || numDepositAmount <= 0) {
+      console.log("Invalid deposit, try again");
+    } else {
+      return numDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
+Number(depositAmount);
+console.log(`Deposited Amount GH₵${depositAmount}.00`);
