a5dfce00be1a89b6da74c47f0b3ce825d87ffd89
ernkeyz <exconzy26@gmail.com>
2024-01-07 08:13:03 +0000

    Project Setup

---
+++ JavaScript/slot_machine/project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if user won
+// 6. Give user award if won else take bet
+// 7. Play again
