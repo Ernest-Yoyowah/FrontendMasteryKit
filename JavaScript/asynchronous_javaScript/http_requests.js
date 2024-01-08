b9c335512af7189c14d0d11508659c91af83f0b5
ernkeyz <exconzy26@gmail.com>
2024-01-08 15:54:38 +0000

    XMLHttpRequest: readyState property

--- JavaScript/asynchronous_javaScript/http_requests.js
+++ JavaScript/asynchronous_javaScript/http_requests.js
@@ -8,3 +8,12 @@ request.addEventListener("readystatechange", () => {
 
 request.open("GET", "https://jsonplaceholder.typicode.com/todos");
 request.send();
+
+// XMLHttpRequest: readyState property
+/*
+1 -	OPENED	open() has been called.
+0 -	UNSENT	Client has been created. open() not called yet.
+2 -	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+3 -	LOADING	Downloading; responseText holds partial data.
+4 -	DONE	The operation is complete.
+*/
