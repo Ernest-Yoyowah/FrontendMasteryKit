e8d2e172860d7b52d32432b3a84519c08926e0b0
ernkeyz <exconzy26@gmail.com>
2024-01-08 17:04:18 +0000

    Callback Functions in action

--- JavaScript/asynchronous_javaScript/callback_functions.js
+++ JavaScript/asynchronous_javaScript/callback_functions.js
@@ -1,18 +1,26 @@
-const getTodos = () => {
+const getTodos = (callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
     //   console.log(request, request.readyState);
 
     if (request.readyState === 4 && request.status === 200) {
-      console.log(request.responseText);
+      //   console.log(request.responseText);
+      callback(undefined, request.responseText);
     } else if (request.readyState === 4) {
-      console.log(`Response Status: ${request.status}`);
+      //   console.log(`Response Status: ${request.status}`);
+      callback(`Response Status: ${request.status}`, undefined);
     }
   });
 
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  request.open("GET", "https://jsonplaceholder.typicode.com/to2dos");
   request.send();
 };
 
-getTodos();
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
