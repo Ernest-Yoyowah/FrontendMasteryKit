69b014f90b0b8b475788b7a22c1cc705a3cb0c92
ernkeyz <exconzy26@gmail.com>
2024-01-08 17:36:47 +0000

    JSON data

---
+++ JavaScript/asynchronous_javaScript/json_data.js
@@ -0,0 +1,27 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    //   console.log(request, request.readyState);
+
+    if (request.readyState === 4 && request.status === 200) {
+      //   console.log(request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      //   console.log(`Response Status: ${request.status}`);
+      callback(`Response Status: ${request.status}`, undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  request.send();
+};
+
+getTodos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
