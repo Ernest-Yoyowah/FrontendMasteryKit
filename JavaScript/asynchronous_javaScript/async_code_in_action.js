ace533c323de93f137f9de35b258ec556467d421
ernkeyz <exconzy26@gmail.com>
2024-01-08 17:15:24 +0000

    Asynchronous programming

--- JavaScript/asynchronous_javaScript/async_code_in_action.js
+++ JavaScript/asynchronous_javaScript/async_code_in_action.js
@@ -1,3 +1,15 @@
+/*
+Asynchronous programming is a technique that enables your program to start a potentially
+long-running task and still be able to be responsive to other events while that task runs,
+rather than having to wait until that task has finished.
+Once that task has finished, your program is presented with the result.
+
+In Short:
+Asynchronous programming enables a program to perform tasks in the background,
+allowing responsiveness to other events, and then handles the results when ready.
+
+*/
+
 console.log(`1`);
 console.log(`2`);
 
