e4255c985fc938d227e1c3bc9e0ccb1eca3d5517
ernkeyz <exconzy26@gmail.com>
2024-01-08 15:59:19 +0000

    XMLHttpRequest

--- JavaScript/asynchronous_javaScript/http_requests.js
+++ JavaScript/asynchronous_javaScript/http_requests.js
@@ -3,7 +3,11 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener("readystatechange", () => {
-  console.log(request, request.readyState);
+  //   console.log(request, request.readyState);
+
+  if (request.readyState === 4) {
+    console.log(`Done`, request);
+  }
 });
 
 request.open("GET", "https://jsonplaceholder.typicode.com/todos");
