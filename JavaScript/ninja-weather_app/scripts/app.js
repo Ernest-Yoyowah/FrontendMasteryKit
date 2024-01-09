16b468e045f1e2d197cfb7d0bcf8353e3cea082c
ernkeyz <exconzy26@gmail.com>
2024-01-09 22:15:41 +0000

    Integrating app.js and forecast.js

--- JavaScript/ninja-weather_app/scripts/app.js
+++ JavaScript/ninja-weather_app/scripts/app.js
@@ -1,9 +1,24 @@
 const cityForm = document.querySelector("form");
 
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return {
+    city: cityDetails,
+    weather: weather,
+  };
+};
+
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // get city value from user input
   const city = cityForm.city.value.trim();
   cityForm.reset();
+
+  // update UI with new city
+  updateCity(city)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
 });
