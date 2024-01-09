c31ddb8cec7ee3707ea790d750f16699f552eec3
ernkeyz <exconzy26@gmail.com>
2024-01-09 21:45:04 +0000

    get city value from user input

--- JavaScript/ninja-weather_app/scripts/app.js
+++ JavaScript/ninja-weather_app/scripts/app.js
@@ -2,4 +2,8 @@ const cityForm = document.querySelector("form");
 
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // get city value from user input
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
 });
