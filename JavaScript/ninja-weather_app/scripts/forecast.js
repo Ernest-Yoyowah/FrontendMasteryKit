a07a8f4786b1672533acdf6f58229a261d83f766
ernkeyz <exconzy26@gmail.com>
2024-01-09 21:12:27 +0000

    weather info api

--- JavaScript/ninja-weather_app/scripts/forecast.js
+++ JavaScript/ninja-weather_app/scripts/forecast.js
@@ -1,5 +1,18 @@
 const key = "dnG9gwgfdAPGccvDewDcCqGa5qa1p9xA";
 
+// weather information
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data;
+};
+
+// City information
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
