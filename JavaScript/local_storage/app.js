af4df0f4a8660e0ad0d4cc1afb7e57c419a1ecb2
ernkeyz <exconzy26@gmail.com>
2024-01-10 21:15:07 +0000

    get data from local storage

--- JavaScript/local_storage/app.js
+++ JavaScript/local_storage/app.js
@@ -1,3 +1,7 @@
 // store data in local storage
 localStorage.setItem("name", "Ernest");
 localStorage.setItem("job", "Software Engineer");
+
+// get data from local storage
+let name = localStorage.getItem("name");
+console.log(name);
