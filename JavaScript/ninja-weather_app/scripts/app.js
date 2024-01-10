e9684913277034a254291f3ab8125e6cc65223a8
ernkeyz <exconzy26@gmail.com>
2024-01-10 20:35:32 +0000

    Ternary Operator

--- JavaScript/ninja-weather_app/scripts/app.js
+++ JavaScript/ninja-weather_app/scripts/app.js
@@ -21,12 +21,12 @@ const updateUI = (data) => {
   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute("src", iconSrc);
 
-  let timeSrc = null;
-  if (weather.IsDayTime) {
-    timeSrc = "img/day.svg";
-  } else {
-    timeSrc = "img/night.svg";
-  }
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  // if (weather.IsDayTime) {
+  //   timeSrc = "img/day.svg";
+  // } else {
+  //   timeSrc = "img/night.svg";
+  // }
   time.setAttribute("src", timeSrc);
 
   // remove d-none class if present
