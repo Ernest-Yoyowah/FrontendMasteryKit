1081082da657fe52d622d53ec7413ce9bb30c268
ernkeyz <exconzy26@gmail.com>
2024-01-10 21:42:35 +0000

    updating data in local storage

--- JavaScript/local_storage/app.js
+++ JavaScript/local_storage/app.js
@@ -4,4 +4,8 @@ localStorage.setItem("job", "Software Engineer");
 
 // get data from local storage
 let name = localStorage.getItem("name");
+
+// updating data in local storage
+localStorage.setItem("name", "Yoyowah");
+name = localStorage.getItem("name");
 console.log(name);
