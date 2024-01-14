93d28ac4af55b06a4b1acb678a16219fdfb5c75c
ernkeyz <exconzy26@gmail.com>
2024-01-14 19:47:08 +0000

    Classes in action

---
+++ JavaScript/object_oriented_javaScript/classes_in_action.js
@@ -0,0 +1,28 @@
+class student {
+  constructor(name, phone, email, programme, program_type, gender, level) {
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.programme = programme;
+    this.program_type = program_type;
+    this.gender = gender;
+    this.level = level;
+  }
+
+  about() {
+    console.log(
+      `${this.name} is a ${this.program_type} ${this.programme} student in level ${this.level}. currently seeking new job opportunities. Contact Phone: ${this.phone} Email: ${this.email}`
+    );
+  }
+}
+
+const student_1 = new student(
+  "Ernest",
+  "0506923484",
+  "ernestniiyoyowah@gmail.com",
+  "Software Engineering",
+  "BSc.",
+  "Male",
+  "200"
+);
+student_1.about();
