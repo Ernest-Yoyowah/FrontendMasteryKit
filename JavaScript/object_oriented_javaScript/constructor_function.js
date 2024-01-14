5987de53685f8f8ccd661cec85971e1c7a1f72f7
ernkeyz <exconzy26@gmail.com>
2024-01-14 22:17:30 +0000

    function constructor

---
+++ JavaScript/object_oriented_javaScript/constructor_function.js
@@ -0,0 +1,10 @@
+// function constructor
+function User(username, email, phone) {
+  this.username = username;
+  this.email = email;
+  this.phone = phone;
+}
+
+const User_1 = new User("Ernest", "ernestniiyoyowah@gmail.com", "0506923484");
+
+console.log(User_1);
