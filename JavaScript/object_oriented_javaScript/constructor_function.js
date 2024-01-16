37b260d4949e918f02bc78ee276f1ec77cc72ccb
ernkeyz <exconzy26@gmail.com>
2024-01-16 23:42:47 +0000

    prototype chain for inheritance Set up

--- JavaScript/object_oriented_javaScript/constructor_function.js
+++ JavaScript/object_oriented_javaScript/constructor_function.js
@@ -16,6 +16,24 @@ User.prototype.logout = function () {
   console.log(`${this.username} just logged out`);
   return this;
 };
+
+// Inherit from User for Admin
+function Admin(username, email, phone, title) {
+  User.call(this, username, email, phone);
+  this.titel = title;
+}
+
+Admin.prototype = Object.create(User.prototype);
+
 const User_1 = new User("Ernest", "ernestniiyoyowah@gmail.com", "0506923484");
 
-User_1.login().logout();
+const admin1 = new Admin(
+  "Ernest",
+  "ernestniiyoyowah@gmail.com",
+  "0506923484",
+  "Administrator"
+);
+
+// User_1.login().logout();
+admin1.login().logout();
+// console.log(admin1);
