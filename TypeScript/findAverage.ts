f62d9dbaf384c4183b4c94bb1393f0dbd32632ef
ernkeyz <exconzy26@gmail.com>
2024-01-23 23:37:57 +0000

    Find Average

--- TypeScript/findAverage.ts
+++ TypeScript/findAverage.ts
@@ -1,5 +1,16 @@
+let sum: number = 0;
+
+const Average = (nums: number[]): number => {
+  for (let i: number = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum / nums.length;
+};
+
 const sampleArray: number[] = [2, 4, 6, 8, 10];
+console.log(Average(sampleArray));
 
-const sum = sampleArray.reduce((acc, val) => acc + val);
+// const sampleArray: number[] = [2, 4, 6, 8, 10];
+// const sum = sampleArray.reduce((acc, val) => acc + val);
 
-console.log(sum / sampleArray.length);
+// console.log(sum / sampleArray.length);
