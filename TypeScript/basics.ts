881d4ba429fb7fceb77dd4b83722a0516705a277
ernkeyz <exconzy26@gmail.com>
2024-01-24 00:03:07 +0000

    TypeScript Basics

---
+++ TypeScript/basics.ts
@@ -0,0 +1,24 @@
+// Primitives: number, string, boolean
+// More Complex types: Arrays, Objects
+// Function types: parameters
+
+// Primitives
+let age: number = 20;
+
+let username: string;
+username = `Ernest`;
+
+let isValid: boolean;
+isValid = true;
+
+// More Complex types: Arrays, Objects
+const nums: number[] = [1, 2, 3, 4, 5, 6];
+
+let person: {};
+
+person = {
+  name: "Ernest",
+  age: 22,
+  gender: "male",
+  role: "Front-End Developer",
+};
