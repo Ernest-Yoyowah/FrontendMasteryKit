705a4eb3e6fa225ede157e71a308ef4384526407
ernkeyz <exconzy26@gmail.com>
2024-01-24 09:50:45 +0000

    Finding max number

---
+++ TypeScript/maxNumber.ts
@@ -0,0 +1,9 @@
+const MaxNumber = (given_numbers: number[]): number => {
+  let max_number = Math.max(...given_numbers);
+  return max_number;
+};
+
+// Example usage:
+const given_numbers: number[] = [42, 17, 8, 94, 25, 62];
+const max_number = MaxNumber(given_numbers);
+console.log("The maximum number is:", max_number); // This should print the maximum number in the array.
