ce759d0ac0f9ad5403bd60ea74cf1091118bde93
ernkeyz <exconzy26@gmail.com>
2024-02-11 20:56:30 +0000

    Problem Statement - Bill Division Challenge - HackerRank

--- TypeScript/billDivision.ts
+++ TypeScript/billDivision.ts
@@ -1,3 +1,20 @@
+// Problem Statement:
+// Two friends Anna and Brian, are deciding how to split the bill at a dinner. Each will only pay for the items they consume.
+// Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
+//
+// For example, assume the bill has the following prices: . Anna declines to eat item  which costs .
+// If Brian calculates the bill correctly, Anna will pay . If he includes the cost of , he will calculate .
+// In the second case, he should refund  to Anna.
+//
+// Function Description:
+// Complete the billDivision function in the editor below. It should print `bill was fairly split` if the bill is fairly split.
+// Otherwise, it should print the integer amount of money that Brian owes Anna.
+//
+// bonAppetit has the following parameter(s):
+// - arrayOfBills: an array of integers representing the cost of each item ordered
+// - indexOfAnnaDecline: an integer representing the zero-based index of the item Anna doesn't eat
+// - annaPaid: the amount of money that Anna contributed to the bill
+
 "use strict";
 
 const billDivision = (
