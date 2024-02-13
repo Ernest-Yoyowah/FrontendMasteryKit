e91c0057b68efce1ff1e70b0ce4620ffce8a8fe0
ernkeyz <exconzy26@gmail.com>
2024-02-13 10:00:25 +0000

    Reverse String Without Built-in Methods

---
+++ TypeScript/reverseString.ts
@@ -0,0 +1,30 @@
+/*
+Algorithm: Reverse String Without Built-in Methods
+
+Input:
+- A string `str`
+
+Output:
+- A string containing the reversed version of `str`
+
+Steps:
+1. Initialize an empty string variable `reversed`.
+2. Iterate through the characters of the input string `str` from the end to the beginning.
+   a. For each character at index `i` (starting from the last character):
+      i. Concatenate the character to the `reversed` string.
+3. Return the `reversed` string.
+
+Pseudocode:
+*/
+
+const reverseStr = (str: string) => {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+};
+
+console.log(reverseStr("Ernest"));
