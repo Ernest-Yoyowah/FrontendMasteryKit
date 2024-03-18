03ffa5a480c0b360aa5c9180e5d06cc767e206c5
ernkeyz <exconzy26@gmail.com>
2024-03-18 13:29:04 +0000

    morning warn up exercise - sum of an array

---
+++ TypeScript/arraySum.ts
@@ -0,0 +1,6 @@
+const arraySum = (arr: number[]): number =>
+  arr.reduce((acc, val) => acc + val, 0);
+
+console.log(arraySum([1, 2, 3, 4, 5])); // Output: 15
+console.log(arraySum([])); // Output: 0
+console.log(arraySum([-1, 0, 1])); // Output: 0
