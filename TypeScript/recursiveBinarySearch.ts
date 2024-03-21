ad6f08996622f90eebc9c243888e3e8a63e5d944
ernkeyz <exconzy26@gmail.com>
2024-03-21 20:00:37 +0000

    warm up code - recusive binary search

---
+++ TypeScript/recursiveBinarySearch.ts
@@ -0,0 +1,29 @@
+const recursiveBinarySearch = (ar: number[], target: number): number => {
+  return search(ar, target, 0, nums.length);
+};
+
+function search(
+  ar: number[],
+  target: number,
+  leftIndex: number,
+  rightIndex: number
+) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+  if (target === ar[midIndex]) {
+    return midIndex;
+  }
+
+  if (target < ar[midIndex]) {
+    return search(ar, target, leftIndex, midIndex - 1);
+  } else {
+    return search(ar, target, midIndex + 1, rightIndex);
+  }
+}
+
+const nums: number[] = [-5, 2, 4, 6, 10];
+console.log(recursiveBinarySearch(nums, 2));
