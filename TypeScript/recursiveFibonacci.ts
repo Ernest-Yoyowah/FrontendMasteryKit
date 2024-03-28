10c4de267bcfc3ddea96bf9ab63bd4b43fb21faa
ernkeyz <exconzy26@gmail.com>
2024-03-28 09:22:06 +0000

    recursive fibonacci sequence

--- TypeScript/recursiveFibonacci.ts
+++ TypeScript/recursiveFibonacci.ts
@@ -1,12 +1,11 @@
 const recursiveFibonacci = (n: number): number => {
-  if (n < 2) {
-    return n;
-  }
+  if (n < 2) return n;
 
   return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 };
 
-console.log(recursiveFibonacci(6));
+let num: number = 6;
+console.log(recursiveFibonacci(num));
 
 // O(n) - Iterative
 // O(2^n) - Recursive
