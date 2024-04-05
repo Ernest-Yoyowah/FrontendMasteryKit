2c971bc7f97856c34cf6c64cc58ecf5a91435b74
ernkeyz <exconzy26@gmail.com>
2024-04-05 22:37:56 +0000

    linear search

--- TypeScript/linearSearch.ts
+++ TypeScript/linearSearch.ts
@@ -1,4 +1,4 @@
-const linearSearch = (array: number[], target: number): number => {
+const linearSearch = (arr: number[], target: number): number => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
       return i;
