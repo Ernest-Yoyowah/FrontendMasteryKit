c0480ab3674fc4da1e0fc486e6d5e39424539a6b
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-12 22:50:51 +0000

    two sum - leetcode

---
+++ LeetCode/two-sum/two_sum.ts
@@ -0,0 +1,35 @@
+/*
+    Problem: Two Sum
+
+    Description:
+    Given an array of integers `nums` and an integer `target`, return the indices of the two numbers 
+    such that they add up to `target`.
+
+    Example:
+    - Input: `nums = [2, 7, 11, 15]`, `target = 9`
+    - Output: `[0, 1]` (Because `nums[0] + nums[1]` is `9`)
+
+    Constraints:
+    - You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    - You can return the answer in any order.
+*/
+
+const twoSum = (nums: number[], target: number): number[] => {
+  const numToIndex: Map<number, number> = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const complement = target - num;
+
+    if (numToIndex.has(complement)) {
+      return [numToIndex.get(complement)!, i];
+    }
+
+    numToIndex.set(num, i);
+  }
+
+  return [];
+};
+
+// Example usage
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
