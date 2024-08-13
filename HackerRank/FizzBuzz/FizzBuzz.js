0eebd7829aab4471eb743776acbf3b4c47dd569f
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-13 20:12:30 +0000

    fix/syntax-error

--- HackerRank/FizzBuzz/FizzBuzz.js
+++ HackerRank/FizzBuzz/FizzBuzz.js
@@ -1,12 +1,17 @@
-const lenght = 15;
-for (let i = 1; i <= lenght; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 == 0) {
-    console.log("Fizz");
-  } else if (i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+// Function to determine the output for each number
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let output = "";
+
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+
+    console.log(output || i);
   }
 }
+
+// Set the length for the FizzBuzz sequence
+const LENGTH = 15;
+
+// Execute the FizzBuzz function
+fizzBuzz(LENGTH);
