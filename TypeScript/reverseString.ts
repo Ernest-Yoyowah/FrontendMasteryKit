867c7ecbbead0c28805e04f7c3a0b88e1e98c0a5
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-13 20:24:42 +0000

    fix/syntax-error

--- TypeScript/reverseString.ts
+++ TypeScript/reverseString.ts
@@ -17,7 +17,7 @@ Steps:
 Pseudocode:
 */
 
-const reverseStr = (str: string) => {
+const reverseStr = (str: string): string => {
   let reversed = "";
 
   for (let i = str.length - 1; i >= 0; i--) {
