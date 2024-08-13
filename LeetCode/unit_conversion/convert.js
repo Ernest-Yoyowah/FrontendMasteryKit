48d8f81907101837c7eaf5d4ea4d86bfdcec2830
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-13 20:07:41 +0000

    fix/syntax-error

--- LeetCode/unit_conversion/convert.js
+++ LeetCode/unit_conversion/convert.js
@@ -1,43 +1,40 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-
 const convertUnit = () => {
-    //declare constants
-    const cm_per_inch = 2.54;
-    const inch_per_feet = 12;
+  //declare constants
+  const cm_per_inch = 2.54;
+  const inch_per_feet = 12;
 
-    // declare variables
-    var feet;
-    var inch;
-    var cm;
+  // declare variables
+  var feet, cm, inch;
 
-    // accept input for feet
-    rl.question("Enter Lenght(feet): ", (feetInput) => {
-        feet = parseFloat(feetInput)
-        console.log(`Lenght in feet: ${feet}`);
+  // accept input for feet
+  rl.question("Enter Lenght(feet): ", (feetInput) => {
+    feet = parseFloat(feetInput);
+    console.log(`Lenght in feet: ${feet}`);
 
     // accept input for  inches
     rl.question("Enter lenght(inches): ", (inchInput) => {
-        inch =  parseFloat(inchInput)
-        console.log(`Lenght in inches ${inch}`);
+      inch = parseFloat(inchInput);
+      console.log(`Lenght in inches ${inch}`);
 
-        // calculate totalInches
-            var totalInches = (feet * inch_per_feet) + inch;
+      // calculate totalInches
+      var totalInches = feet * inch_per_feet + inch;
 
-        // calculate conversion
-            cm = (totalInches * cm_per_inch);
+      // calculate conversion
+      cm = totalInches * cm_per_inch;
 
-        // output the result
-            console.log(`Lenght in Centimeters: ${cm.toFixed(2)}`);
+      // output the result
+      console.log(`Lenght in Centimeters: ${cm.toFixed(2)}`);
 
-            rl.close();
-        });
+      rl.close();
     });
+  });
 };
 
 convertUnit();
