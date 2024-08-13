dbe7a2a656fe821cb638b8cb67273e9a8a40ab2f
Ernest Yoyowah <ernestniiyoyowah@gmail.com>
2024-08-13 20:11:09 +0000

    fix/syntax-error

--- LeetCode/two-sum/two_sum.ts
+++ LeetCode/two-sum/two_sum.ts
@@ -15,7 +15,7 @@
 */
 
 const twoSum = (nums: number[], target: number): number[] => {
-  const numToIndex = new Map();
+  const numToIndex: Map<number, number> = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
