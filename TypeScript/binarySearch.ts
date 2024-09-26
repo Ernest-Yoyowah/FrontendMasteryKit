39e1264a0045ac8edab7f5dbc99198af9f70441a
Ernest Yoyowah <117729383+Ernest-Yoyowah@users.noreply.github.com>
2024-09-26 22:07:23 +0000

    Update binarySearch.ts

--- TypeScript/binarySearch.ts
+++ TypeScript/binarySearch.ts
@@ -1,6 +1,5 @@
 /**
  * Performs a binary search on a sorted array to find the target value.
- *
  * @param {number[]} arr - The sorted array to search in.
  * @param {number} target - The value to search for.
  * @returns {number} The index of the target value if found, otherwise -1.
